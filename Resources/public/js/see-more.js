26c2f304ebdb12535f8ed06f96b30972f68367c2
pkshetlie <pierrick.pobelle@gmail.com>
2019-08-13 21:32:36 +0200

    we can now choose the parent for the seemore Option

--- Resources/public/js/see-more.js
+++ Resources/public/js/see-more.js
@@ -1,11 +1,16 @@
 $(function(){
     $(document).on('click','.btn-paginatation.see-more',function(){
        let $t = $(this);
+       var parentElt = $($t.data('parent'));
         $.ajax({
             url : $t.data('href'),
             dataType: 'html',
             success: function(data){
-                $t.parent().append(data);
+                if(parentElt === undefined) {
+                    $t.parent().append(data);
+                }else{
+                    parentElt.append(data);
+                }
                 $t.remove();
             }
         });
