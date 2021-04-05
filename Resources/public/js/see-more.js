db597dbb27ac1dd6210a867be7ff3da999941b71
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-05 22:47:17 +0200

    ajout de quelques fonctionnalités javascript

--- Resources/public/js/see-more.js
+++ Resources/public/js/see-more.js
@@ -1,6 +1,6 @@
 $(function(){
     $(document).on('click','.btn-paginatation.see-more',function(){
-       let $t = $(this);
+        let $t = $(this);
         $.ajax({
             url : $t.data('href'),
             dataType: 'html',
@@ -11,9 +11,13 @@ $(function(){
                     let parentElt = $($t.data('parent'));
                     parentElt.append(data);
                 }
+                if($t.closest(".see-more-delete") !== undefined) {
+                    $t.closest(".see-more-delete").remove();
+                }
                 $t.remove();
+                $(document).trigger('seemore.remove');
             }
         });
-       return false;
+        return false;
     });
 });
