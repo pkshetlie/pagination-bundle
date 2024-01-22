88480860279cb3a490aab28fef45e08aa0d829c9
Rhokapa <pierrick.pobelle@gmail.com>
2024-01-22 22:06:03 +0100

    fix page < 0 or not numeric

--- Service/PaginationService.php
+++ Service/PaginationService.php
@@ -22,7 +22,9 @@ class PaginationService
 
     /**
      * define the default number of element by page
+     *
      * @param int $nb_elt_per_page
+     *
      * @return $this
      */
     public function setDefaults($nb_elt_per_page = 25)
@@ -30,36 +32,53 @@ class PaginationService
         if ($nb_elt_per_page !== null) {
             $this->nb_elt_per_page = $nb_elt_per_page;
         }
+
         return $this;
     }
 
     /**
      * @param QueryBuilder $queryBuilder
      * @param Request $request
+     *
      * @return Pagination
      */
     public function process(QueryBuilder $queryBuilder, Request $request)
     {
 
         $pagination = new Pagination();
-        $pagination->setLastEntityId($request->get('plentid' . $pagination->getIdentifier(), 0));
-        $pagination->setIsPartial($request->get('ppartial' . $pagination->getIdentifier(), false));
+        $pagination->setLastEntityId($request->get('plentid'.$pagination->getIdentifier(), 0));
+        $pagination->setIsPartial($request->get('ppartial'.$pagination->getIdentifier(), false));
         $usableQuery = clone $queryBuilder;
-        $page = $request->get('ppage' . $pagination->getIdentifier(), 1) - 1;
+        $page = $request->get('ppage'.$pagination->getIdentifier(), 1);
 
+        if (!is_numeric($page)) {
+            $page = 1;
+        }
+
+        $page -= 1;
 
         $startAt = $page * $this->nb_elt_per_page;
+
         try {
-            $countRslt = $usableQuery->addSelect('COUNT( DISTINCT ' . $usableQuery->getAllAliases()[0] . ') as count_nb_elt')->getQuery()->getOneOrNullResult();
+            $countRslt = $usableQuery->addSelect(
+                'COUNT( DISTINCT '.$usableQuery->getAllAliases()[0].') as count_nb_elt'
+            )->getQuery()->getOneOrNullResult();
         } catch (Exception $e) {
             $usableQuery = clone $queryBuilder;
-            $countRsltat = $usableQuery->addSelect('COUNT( DISTINCT ' . $usableQuery->getAllAliases()[0] . ') as count_nb_elt')->getQuery()->getResult();
+            $countRsltat = $usableQuery->addSelect(
+                'COUNT( DISTINCT '.$usableQuery->getAllAliases()[0].') as count_nb_elt'
+            )->getQuery()->getResult();
             $countRslt['count_nb_elt'] = count($countRsltat);
         }
+
         $nb_pages = ceil(($countRslt != null ? $countRslt['count_nb_elt'] : 0) / $this->nb_elt_per_page);
-        $nb_pages = $nb_pages < 0 ? 0 : $nb_pages;
-        $startAt = $startAt < 0 ? 0 : $startAt;
-        $entities = $queryBuilder->setMaxResults($this->nb_elt_per_page)->setFirstResult($startAt)->getQuery()->getResult();
+        $nb_pages = max($nb_pages, 0);
+        $startAt = max($startAt, 0);
+
+        $entities = $queryBuilder
+            ->setMaxResults($this->nb_elt_per_page)
+            ->setFirstResult($startAt)
+            ->getQuery()->getResult();
 
         $pagination->setEntities($entities);
         $pagination->setPages($nb_pages);
